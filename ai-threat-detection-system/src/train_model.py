9904babed51e7a049c6db3418bef17182de9cfec
aaa-aashna <unityy7089@gmail.com>
2026-06-07 00:08:22 +0530

    mhm

--- ai-threat-detection-system/src/train_model.py
+++ ai-threat-detection-system/src/train_model.py
@@ -3,6 +3,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("data/spam.csv")
 
